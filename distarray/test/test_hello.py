f486548898cb9bd973f074a021c127bbabe07d8a
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2012-09-04 10:47:51 +0200

    Support for unit-testing with MPI

--- distarray/test/test_hello.py
+++ distarray/test/test_hello.py
@@ -1,5 +1,13 @@
+from .common import *
 
 import distarray.hello
 
-def test_hello():
+# run with, e.g., mpiexec -np 10 python runtests.py --nocapture
+
+@mpi(4)
+def test_hello(comm):
+    print_in_turn(comm, 'hello')
+    if comm.Get_rank() == 2:
+        1/0
     assert 'Hello World!' == distarray.hello.hello()
+
